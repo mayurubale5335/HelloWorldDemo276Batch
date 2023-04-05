c5bb208c603516a3ebce49cb8818d7428379eb82
unknown <mayurubale5335@gmail.com>
2023-04-05 09:17:30 +0530

    [mayur ubale] Adding welcome message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo276Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 276 Batch");
+            Console.ReadLine();
+
+        }
+    }
+}
