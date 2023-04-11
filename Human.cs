bbeed7ba04f79224ec91612e6e8916549922504e
unknown <mayurubale5335@gmail.com>
2023-04-11 22:14:33 +0530

    [mayur ubale] Adding Human Program with instance members

---
+++ Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo276Batch
+{
+   public class Human
+    {
+        //instance variables
+        //name,height,weight,Age
+        //syntax - access modifier datatype variables
+        public string name;
+        public float height,weight;
+        public int age;
+        
+        //instance method
+        public void talk()   //method signature/defination
+        {
+            // method body or implementation
+            Console.WriteLine("Human can talk");
+        }
+
+        public void HumanDetails(string humanName, float humanHeight, float humanWeight, int age)   //method signature/defination
+        {
+            // method body or implementation
+            Console.WriteLine("Human Details:{0} {1} {2} {3}", humanName, humanHeight, humanWeight, age);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,11 +12,21 @@ namespace HelloWorldDemo276Batch
         {
             Console.WriteLine("Welcome to 276 Batch");
             Console.WriteLine("Code pushing from 2nd Developer");
-
             Console.WriteLine("Modified in Local Repository");
-
             Console.WriteLine("Modified from Remote Repository");
-
+            // object or instance syntax
+            // ClassName variable = new ClassName();
+            Human human = new Human();
+            Console.WriteLine("please enter human name");
+            human.name = Console.ReadLine();
+            Console.WriteLine("please enter human height");
+            human.height =Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("please enter human weight");
+            human.weight = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("please enter human age");
+            human.age = Convert.ToInt32(Console.ReadLine());
+            human.talk();
+            human.HumanDetails(human.name, human.height, human.weight, human.age);
             Console.ReadLine();
 
         }
